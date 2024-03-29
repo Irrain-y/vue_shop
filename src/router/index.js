fb8b2ccaef0ed6b3dc647f47b7f258a15e2797dd
irrain <irrain.yy@gmail.com>
2024-03-30 00:34:11 +0900

    完成了登陆功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginView from '../components/LoginView.vue'
+import HomeView from '../components/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: LoginView }
+    { path: '/login', component: LoginView },
+    { path: '/home', component: HomeView }
   ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next() 放行     next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
